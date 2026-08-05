85f4be6b40513947b11cc3bd0d2d6e4271caaafc
Tangguh Darwisy <tdarwisyy@gmail.com>
2026-08-05 09:57:39 +0700

    Additional Output

--- Bilangan.py
+++ Bilangan.py
@@ -4,3 +4,6 @@ if x % 2 == 0:
     print(x, "adalah bilangan genap")
 else:
     print(x, "adalah bilangan ganjil")
+
+print("Terima kasih telah menggunakan program ini.")
+print("===========================================")
