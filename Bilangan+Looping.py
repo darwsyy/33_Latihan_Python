1f5f44b9216aca4a357c1c4e1ed4f646bec5d61f
Tangguh Darwisy <tdarwisyy@gmail.com>
2026-08-10 14:24:40 +0700

    Add Looping

---
+++ Bilangan+Looping.py
@@ -0,0 +1,15 @@
+print("<============ Program untuk menentukan GANJIL/GENAP ============>")
+while True:
+    x = int(input("Masukkan bilangan yang akan diperiksa: "))
+
+    if x % 2 == 0:
+        print(x, "adalah bilangan genap 🟢")
+    else:
+        print(x, "adalah bilangan ganjil 🔴")
+        
+    if input("\nApakah ingin memeriksa bilangan lain? (Y/N):").upper() == "N":
+        print("Terima kasih telah menggunakan program ini.")
+        print("================================================================")
+        break
+    else:
+        continue
--- Bilangan.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Masukkan bilangan: "))
-
-if x % 2 == 0:
-    print(x, "adalah bilangan genap")
-else:
-    print(x, "adalah bilangan ganjil")
-
-print("Terima kasih telah menggunakan program ini.")
-print("===========================================")
