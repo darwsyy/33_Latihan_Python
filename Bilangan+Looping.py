786182fbd0298e3e1eaabfb74c68a45bb0c24046
Tangguh Darwisy <tdarwisyy@gmail.com>
2026-08-24 15:02:19 +0700

    Ganjil Genap Function

--- Bilangan+Looping.py
+++ Bilangan+Looping.py
@@ -18,5 +18,33 @@ def ganjil_genap():
             break
         else:
             continue
-    
+
+
+def cek_prima():
+    while True:
+        angka = int(input("Masukkan bilangan yang akan diperiksa: "))
+
+        if angka < 2:
+            print(angka, "bukan bilangan prima")
+        else:
+            prima = True
+
+            for pembagi in range(2, int(angka ** 0.5) + 1):
+                if angka % pembagi == 0:
+                    prima = False
+                    break
+
+            if prima:
+                print(angka, "adalah bilangan prima")
+            else:
+                print(angka, "bukan bilangan prima")
+
+        if input("\nApakah ingin memeriksa bilangan lain? (Y/N): ").upper() == "N":
+            print("Terima kasih telah menggunakan program ini.")
+            break
+
+
+cek_prima()
+
+
 ganjil_genap()
