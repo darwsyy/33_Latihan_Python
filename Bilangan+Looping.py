9e47ef8c3e3bd4c2dc0ececcbd716d3b0350bd97
Tangguh Darwisy <tdarwisyy@gmail.com>
2026-08-24 14:16:29 +0700

    Kelas

--- Bilangan+Looping.py
+++ Bilangan+Looping.py
@@ -1,3 +1,18 @@
-print("Halo Bos")
+import os
+print(os.system("cls"))
 
-print("Halo Bos2")
+print("<============ Program untuk menentukan GANJIL/GENAP ============>")
+while True:
+    x = int(input("Masukkan bilangan yang akan diperiksa: "))
+
+    if x % 2 == 0:
+        print(x, "adalah bilangan genap 🟢")
+    else:
+        print(x, "adalah bilangan ganjil 🔴")
+        
+    if input("\nApakah ingin memeriksa bilangan lain? (Y/N):").upper() == "N":
+        print("Terima kasih telah menggunakan program ini.")
+        print("================================================================")
+        break
+    else:
+        continue
